08da9d5265d35469ca40c380ee58bd227377ebfa
Rick South <rick@natamus.com>
2023-12-22 18:18:43 +0100

    (v4.6) Moved mixin files to the Common source folder to make future development easier.

--- Common/src/main/java/com/natamus/passiveendermen/util/Reference.java
+++ Common/src/main/java/com/natamus/passiveendermen/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.passiveendermen.util;
 public class Reference {
 	public static final String MOD_ID = "passiveendermen";
 	public static final String NAME = "Passive Endermen";
-	public static final String VERSION = "4.5";
+	public static final String VERSION = "4.6";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }
