cdf3735137fcf68c16cbb00836b25376373da233
Rick South <rick@natamus.com>
2024-07-28 07:21:53 +0200

    (v4.8) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/passiveendermen/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/passiveendermen/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.passiveendermen;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.passiveendermen.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.passiveendermen.util.Reference;
 import net.neoforged.bus.api.IEventBus;
@@ -13,6 +14,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
