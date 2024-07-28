6c896ae20223bebc3346633aa217ab6f0de324e3
Rick South <rick@natamus.com>
2024-07-28 07:21:49 +0200

    (v4.8) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/passiveendermen/ModForge.java
+++ Forge/src/main/java/com/natamus/passiveendermen/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.passiveendermen;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.passiveendermen.forge.config.IntegrateForgeConfig;
 import com.natamus.passiveendermen.util.Reference;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,6 +14,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
