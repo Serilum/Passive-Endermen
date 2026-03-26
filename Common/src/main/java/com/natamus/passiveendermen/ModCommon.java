4c86a041de8194d8feddfed87587c1a2330b74a0
Rick South <rick@natamus.com>
2026-03-26 01:37:14 +0100

    Passive Endermen source code for 26.1.0.

---
+++ Common/src/main/java/com/natamus/passiveendermen/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.passiveendermen;
+
+import com.natamus.passiveendermen.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
