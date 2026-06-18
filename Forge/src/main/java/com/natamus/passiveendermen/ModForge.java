148491e853cceee69cd6be80ebfbafb66a271bc9
Rick South <rick@natamus.com>
2026-06-18 23:34:15 +0200

    Updated Passive Endermen to Minecraft version 26.2.0.

--- Forge/src/main/java/com/natamus/passiveendermen/ModForge.java
+++ Forge/src/main/java/com/natamus/passiveendermen/ModForge.java
@@ -5,7 +5,6 @@ import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.passiveendermen.forge.config.IntegrateForgeConfig;
 import com.natamus.passiveendermen.util.Reference;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
